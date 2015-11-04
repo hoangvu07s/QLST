27685a61a9b6e117c00ae4314001e633807395e2
thanhtam <phantam2501@gmail.com>
2015-11-04 22:52:44 +0700

    a

--- Database/Tables/DoiTraHangHoa.sql
+++ Database/Tables/DoiTraHangHoa.sql
@@ -3,7 +3,6 @@
 	[DoiTraHangHoaId] uniqueidentifier NOT NULL PRIMARY KEY, 
     [HoaDonId] UNIQUEIDENTIFIER NOT NULL, 
 	[NhanVienId] BIGINT NOT NULL,
-	[KhoId] BIGINT NOT NULL,
 	[NgayLap] DATETIME2 NOT NULL,
     [TongTienTraLai] DECIMAL(18, 2) NOT NULL,
     [NgayChinhSua] DATETIME2 NULL DEFAULT SYSDATETIME(), 
@@ -11,6 +10,5 @@
     [HoatDong] BIT NULL DEFAULT 1, 
     CONSTRAINT [FK_DoiTraHangHoa_HoaDon] FOREIGN KEY ([HoaDonId]) REFERENCES [HoaDon]([HoaDonId]),
 	CONSTRAINT [FK_DoiTraHangHoa_NhanVien] FOREIGN KEY ([NhanVienId]) REFERENCES [NhanVien]([Id]),
-	CONSTRAINT [FK_DoiTraHangHoa_Kho] FOREIGN KEY ([KhoId]) REFERENCES [Kho]([Id]),
 	CONSTRAINT [FK_DoiTraHangHoa_NguoiChinhSua] FOREIGN KEY ([NguoiChinhSuaId]) REFERENCES [NhanVien]([Id])
 )
