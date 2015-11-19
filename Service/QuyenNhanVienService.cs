bffbff96964ce3d775e6d85017656777fff7dd3d
hoangvu07s <hoangvu07s@gmail.com>
2015-11-20 05:53:27 +0700

    fgh

---
+++ Service/QuyenNhanVienService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helper;
+using Model;
+
+namespace Service
+{
+    public class QuyenNhanVienService : BaseService
+    {
+        public QuyenNhanVienService(QuanLySieuThiEntities quanLySieuThiEntities)
+        {
+            Entities = quanLySieuThiEntities;
+        }
+
+        public IList<QuyenNhanVien> GetAll()
+        {
+            try
+            {
+                return Entities.QuyenNhanViens.Where(_=>_.HoatDong).Include(_ => _.NhanVien).Include(_ => _.Quyen).ToList();
+            }
+            catch (Exception ex)
+            {
+                QuanLySieuThiHelper.LogError(ex);
+            }
+
+            return null;
+        }
+
+        public QuyenNhanVien AddQuyenNhanVien()
+        {
+            try
+            {
+                return Entities.QuyenNhanViens.Add(new QuyenNhanVien());
+            }
+            catch (Exception ex)
+            {
+                QuanLySieuThiHelper.LogError(ex);
+            }
+
+            return null;
+        }
+
+        public QuyenNhanVien GetByNhanVienId(long nhanVienId)
+        {
+            try
+            {
+                return Entities.QuyenNhanViens.FirstOrDefault(_ => _.NhanVienId == nhanVienId && _.HoatDong);
+            }
+            catch (Exception ex)
+            {
+                QuanLySieuThiHelper.LogError(ex);
+            }
+
+            return null;
+        }
+
+        public void Update(QuyenNhanVien quyenNhanVien)
+        {
+            try
+            {
+                Entities.QuyenNhanViens.AddOrUpdate(quyenNhanVien);
+            }
+            catch (Exception ex)
+            {
+                QuanLySieuThiHelper.LogError(ex);
+            }
+        }
+
+        public void Delete(QuyenNhanVien quyenNhanVien)
+        {
+            try
+            {
+                quyenNhanVien.HoatDong = false;
+
+                Entities.QuyenNhanViens.AddOrUpdate(quyenNhanVien);
+            }
+            catch (Exception ex)
+            {
+                QuanLySieuThiHelper.LogError(ex);
+            }
+        }
+
+        public override void Save()
+        {
+            Entities.SaveChanges();
+        }
+    }
+}
---
+++ Service/QuyenService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Helper;
+using Model;
+
+namespace Service
+{
+    public class QuyenService : BaseService
+    {
+        public QuyenService(QuanLySieuThiEntities quanLySieuThiEntities)
+        {
+            Entities = quanLySieuThiEntities;
+        }
+
+        public IList<Quyen> GetAll()
+        {
+            try
+            {
+                return Entities.Quyens.ToList();
+            }
+            catch (Exception ex)
+            {
+                QuanLySieuThiHelper.LogError(ex);
+            }
+
+            return null;
+        }
+    }
+}
